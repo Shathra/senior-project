65ca71411592e9a731a002a4cb7f36a37c6277c9
unknown <atesbalci@gmail.com>
2015-10-27 14:12:45 +0200

    complete animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,47 @@ public class Player : MonoBehaviour {
     public GameObject rockPrefab;
 
     private Animator anim;
+    private BoxCollider2D hitbox;
+    private Rigidbody2D body;
 
     void Start() {
         anim = GetComponent<Animator>();
+        hitbox = GetComponentInChildren<BoxCollider2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
         float crouching = anim.GetFloat("Crouching");
+        bool ground = false;
+        Vector2 hitboxBottom = new Vector2(hitbox.bounds.min.x, hitbox.bounds.min.y);
+        RaycastHit2D[] result = new RaycastHit2D[1];
+        if (Physics2D.LinecastNonAlloc(hitboxBottom, hitboxBottom - Vector2.up * 0.02f, result, LayerMask.GetMask("Obstacle")) > 0)
+            ground = true;
+        else {
+            hitboxBottom += Vector2.right * hitbox.size.x;
+            if (Physics2D.LinecastNonAlloc(hitboxBottom, hitboxBottom - Vector2.up * 0.02f, result, LayerMask.GetMask("Obstacle")) > 0)
+                ground = true;
+        }
+        anim.SetBool("Ground", ground);
 
-        float speed = 5 - crouching * 2.5f;
-        if (Input.GetKey(KeyCode.A)) {
-            transform.position += Vector3.left * Time.deltaTime * speed;
-            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, 270, transform.rotation.eulerAngles.z);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
+            float angle = 90;
+            float speed = Time.deltaTime * (5 - crouching * 2.5f);
+            if (Input.GetKey(KeyCode.A)) {
+                angle = 270;
+                speed = -speed;
+            }
+            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, angle, transform.rotation.eulerAngles.z);
+            transform.position += speed * Vector3.right;
             anim.SetFloat("Movement", 1);
-        } else if (Input.GetKey(KeyCode.D)) {
-            transform.position += Vector3.right * Time.deltaTime * speed;
-            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, 90, transform.rotation.eulerAngles.z);
-            anim.SetFloat("Movement", 1);
-        } else
+        } else {
             anim.SetFloat("Movement", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && ground)
+            body.velocity += new Vector2(0, 8 - crouching * 1);
+        if (!ground && !Input.GetKey(KeyCode.Space) && body.velocity.y > 0)
+            body.velocity = new Vector2(body.velocity.x, 0);
 
         if (Input.GetKeyDown(KeyCode.C))
             anim.SetFloat("Crouching", Mathf.Abs(crouching - 1));
