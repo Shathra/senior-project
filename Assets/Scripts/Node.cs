70ae883421c2a25cb93b0216f319eb18ceb1c60f
Firat Ozbay <firat.ozbay@ug.bilkent.edu.tr>
2015-12-15 22:05:02 +0200

    FIRAT WUS HERE -Graph class -Commentleri okumayin

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node : MonoBehaviour {
+    public int[] properties;
+    public Node[] edges;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void AddEdge(Node n)
+    {
+
+    }
+}
