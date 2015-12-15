b209be1fb5c20e146389599d3fcbf062f29b4999
unknown <atesbalci@gmail.com>
2015-12-15 20:13:13 +0200

    added action along with various bug fixes

---
+++ Assets/Classes/Action.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Action {
+    public int priority { get; set; }
+
+    public Action(int priority = 0) {
+        this.priority = priority;
+    }
+
+    public virtual void Execute(Enemy enemy) {
+
+    }
+}
