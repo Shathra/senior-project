97216626e195e51d424fa248912436d08ef6fb0e
Firat Ozbay <firat.ozbay@ug.bilkent.edu.tr>
2016-02-22 20:48:20 +0200

    Defined State class and Statuses of the enemy

--- Assets/Classes/AI/State.cs
+++ Assets/Classes/AI/State.cs
@@ -3,9 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+public enum Status
+{
+    Idle = 1,       // Player has not been seen once
+    HasSeen = 2,    // Player has been seen at least once but evaded enemies
+    Caution = 3,    // Enemy is suspicious of a noise
+    Searching = 4,  // Player has been seen but whereabouts are lost, enemies search whole local area
+    Hostile = 5     // Player is seen and all enemies are alert
+}
+
 /// <summary>
 /// Represent state of an enemy (Idle, searching, hostile etc.)
 /// </summary>
 public class State {
+    Status stt;
+    public State() {
+        stt = Status.Idle;
+    }
+    public Status GetState()
+    {
+        return stt;
+    }
+    public void SetStates(Status s)
+    {
+        stt = s;
+    }
 }
-
