e690a3202131b5504345a66e7dddccf06b20fb9d
Ateş Balcı <atesbalci@gmail.com>
2016-03-19 23:33:00 +0200

    camera vision updated

--- Assets/Classes/SecurityCameraVision.cs
+++ Assets/Classes/SecurityCameraVision.cs
@@ -2,17 +2,5 @@
 using System.Collections;
 
 public class SecurityCameraVision : MonoBehaviour {
-	public SecurityCamera cam {	get; set; }
-    void Start()
-    {
-        cam = transform.parent.gameObject.GetComponent<SecurityCamera>();
-    }
-	void OnTriggerEnter2D(Collider2D col) {
-		if (col.gameObject.tag == "Player") {
-			Player player = col.gameObject.GetComponent<Player>();
-            if (Physics2D.Raycast(transform.position, player.midPoint - (Vector2)transform.position, SecurityCamera.RANGE, LayerMask.GetMask("Obstacle", "Player")).collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
-				cam.Spot(player);
-			}
-		}
-	}
+
 }
