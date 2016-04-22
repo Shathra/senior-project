f28e0b06befe15a4fe9e10726d3b1099bee338a0
Ateş Balcı <atesbalci@gmail.com>
2016-04-23 00:43:05 +0300

    added ability to browse map with mouse (shift to browse longer distance)

---
+++ Assets/Classes/MainCameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraController : MonoBehaviour {	
+	void Update () {
+        Vector3 mouseRelativeToPlayer = Camera.main.ScreenToWorldPoint(Input.mousePosition)
+            - transform.parent.position;
+        mouseRelativeToPlayer = new Vector3(mouseRelativeToPlayer.x, mouseRelativeToPlayer.y, transform.position.z);
+        transform.localPosition = Vector3.Lerp(new Vector3(0, 0, transform.position.z), mouseRelativeToPlayer,
+            Input.GetKey(KeyCode.LeftShift) ? 0.5f : 0.25f);
+	}
+}
