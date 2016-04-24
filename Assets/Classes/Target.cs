0f8939dbcd94b8f13aaa7a5c91111cf84616e801
Ateş Balcı <atesbalci@gmail.com>
2016-04-25 01:59:00 +0300

    target prefab change

--- Assets/Classes/Target.cs
+++ Assets/Classes/Target.cs
@@ -1,24 +1,62 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Target : MonoBehaviour {
-    public Sprite sprite1;
-    public Sprite sprite2;
-	// Use this for initialization
-	void Start () {
-        gameObject.GetComponent<SpriteRenderer>().sprite = sprite1;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float requiredTime { get; set; }
+
+	public Sprite sprite1;
+	public Sprite sprite2;
+
+	private Text timerText;
+	private Image timerImage;
+	private float timer;
+	private bool playerIn;
+	private bool _taken;
+	private bool taken {
+		get {
+			return _taken;
+		}
+		set {
+			if (_taken == false && value == true) {
+				Player.instance.carriesTarget = true;
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprite2;
+			}
+			_taken = value;
+		}
 	}
 
-    void OnTriggerEnter2D(Collider2D col) {
-        if(col.gameObject.tag == "Player")
-        {
-            col.gameObject.GetComponent<Player>().carriesTarget = true;
-            gameObject.GetComponent<SpriteRenderer>().sprite = sprite2;
-        }
-    }
+	// Use this for initialization
+	void Start() {
+		gameObject.GetComponent<SpriteRenderer>().sprite = sprite1;
+		requiredTime = 3;
+		playerIn = false;
+		timer = requiredTime;
+		taken = false;
+		timerText = GetComponentInChildren<Text>();
+		timerImage = GetComponentInChildren<Image>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (playerIn)
+			timer -= Time.deltaTime;
+		if (timer <= 0)
+			taken = true;
+		timerText.text = (playerIn && !taken) ? "" + Mathf.Round(timer) : "";
+		timerImage.fillAmount = (playerIn && !taken) ? timer / requiredTime : 0.0f;
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			playerIn = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			playerIn = false;
+			timer = requiredTime;
+		}
+	}
 }
