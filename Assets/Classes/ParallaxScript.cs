01624af5a80d7dc4108b7ebfb1fc28a0682d7504
Firat Ozbay <firat.ozbay@ug.bilkent.edu.tr>
2016-04-24 23:46:12 +0300

    Parallax and Ladder fixed

--- Assets/Classes/ParallaxScript.cs
+++ Assets/Classes/ParallaxScript.cs
@@ -9,9 +9,9 @@ public class ParallaxScript : MonoBehaviour {
     public float parallaxCoefficent;
     
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         displacement = Player.instance.lastPosition.x - Player.instance.transform.position.x;
         yDisplacement = Player.instance.lastPosition.y - Player.instance.transform.position.y;
-        transform.Translate(new Vector3(displacement/(parallaxCoefficent*5), -yDisplacement/ (parallaxCoefficent * 5), 0));
+        transform.Translate(new Vector3(displacement/(parallaxCoefficent*10), yDisplacement, 0));
     }
 }
