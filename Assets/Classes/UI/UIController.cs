40fc26a149c69dbedaccbd89b38eea58ec731656
Firat Ozbay <firat.ozbay@ug.bilkent.edu.tr>
2016-04-25 09:11:41 +0300

    Atessss

--- Assets/Classes/UI/UIController.cs
+++ Assets/Classes/UI/UIController.cs
@@ -9,12 +9,12 @@ public class UIController : MonoBehaviour {
 	}
 
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.LeftControl)) {
+		if (Input.GetKeyDown(KeyCode.Tab)) {
 			skillWheel.SetActive(true);
 			Player.instance.playerLock = true;
 			Time.timeScale = 0;
 		}
-		if (Input.GetKeyUp(KeyCode.LeftControl)) {
+		if (Input.GetKeyUp(KeyCode.Tab)) {
 			skillWheel.SetActive(false);
 			Player.instance.playerLock = false;
 			Time.timeScale = 1;
