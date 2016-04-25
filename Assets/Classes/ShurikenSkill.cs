c86d324b3db046ee293c1811d916b0c52501c6a6
Ateş Balcı <atesbalci@gmail.com>
2016-04-25 06:36:55 +0300

    added new skill system and wheel

---
+++ Assets/Classes/ShurikenSkill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ShurikenSkill : Skill {
+	private GameObject shurikenPrefab;
+
+	public ShurikenSkill() : base() {
+		charges = 4;
+		name = "Shuriken";
+		description = "Despite the implication of its name, " +
+			"Prometheus uses this seemingly uncivilized weapon " +
+			"to distract his enemies.";
+		shurikenPrefab = Resources.Load<GameObject>("Shuriken");
+	}
+
+	public override void Cast(Vector2 target) {
+		Vector2 direction = target - player.midPoint;
+		Rigidbody2D shuriken = ((GameObject)MonoBehaviour.Instantiate(shurikenPrefab,
+			player.midPoint, Quaternion.identity)).GetComponent<Rigidbody2D>();
+		shuriken.AddForce(direction.normalized * 80);
+	}
+}
---
+++ Assets/Classes/UI/UISelectedSkıll.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UISelectedSkıll : MonoBehaviour {
+	public Text description;
+
+	private SkillSet skillSet;
+	private int cursor;
+
+	void Start() {
+		skillSet = Player.instance.skillSet;
+	}
+
+	void Update() {
+		Vector2 mousePos = new Vector2(Input.mousePosition.x - Screen.width / 2,
+			Input.mousePosition.y - Screen.height / 2);
+		float angle = Vector2.Angle(Vector2.up, mousePos.normalized);
+		if (mousePos.x < 0)
+			angle = 360 - angle;
+		cursor = Mathf.RoundToInt(angle) / 60;
+		transform.rotation = Quaternion.Lerp(transform.rotation,
+			Quaternion.Euler(0, 0, 360 - (60 * cursor)),
+			0.2f);
+		description.text = "";
+		if (skillSet.skills.Length > cursor)
+			description.text = skillSet.skills[cursor].description;
+
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+			skillSet.selectedSkill = cursor;
+	}
+}
